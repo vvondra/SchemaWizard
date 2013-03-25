60ac7adb240e5826d4bca19daa9efa2b1ba46adf
Vojtech Vondra <vojtavondra@gmail.com>
2013-03-25 22:21:15 +0100

    Javadoc update

---
+++ src/cz/vojtechvondra/schemawizard/gui/DecompositionFrame.java
@@ -0,0 +1,11 @@
+package cz.vojtechvondra.schemawizard.gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vojta
+ * Date: 25.3.13
+ * Time: 22:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class DecompositionFrame {
+}

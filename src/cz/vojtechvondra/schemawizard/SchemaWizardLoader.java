82fd2a413265ecc2e0440057f44d4d8314abdc11
Vojtech Vondra <vojtavondra@gmail.com>
2013-03-25 21:09:12 +0100

    Use invokeLater for GUI init

--- src/cz/vojtechvondra/schemawizard/SchemaWizardLoader.java
+++ src/cz/vojtechvondra/schemawizard/SchemaWizardLoader.java
@@ -2,9 +2,20 @@ package cz.vojtechvondra.schemawizard;
 
 import cz.vojtechvondra.schemawizard.gui.WorkspaceFrame;
 
+import javax.swing.*;
+
 public class SchemaWizardLoader {
 
 	static public void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				launchGui();
+			}
+		});
+	}
+
+	static protected void launchGui() {
 		WorkspaceFrame gui = new WorkspaceFrame();
 		gui.create();
 	}

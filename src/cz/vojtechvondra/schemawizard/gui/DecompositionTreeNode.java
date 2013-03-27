398590a2384e6e085372c46a861a74af4ec73ddf
Vojtech Vondra <vojtavondra@gmail.com>
2013-03-27 13:36:50 +0100

    Javadoc

---
+++ src/cz/vojtechvondra/schemawizard/gui/DecompositionTreeNode.java
@@ -0,0 +1,11 @@
+package cz.vojtechvondra.schemawizard.gui;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vojta
+ * Date: 27.3.13
+ * Time: 9:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class DecompositionTreeNode {
+}
